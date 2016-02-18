899898ad1930314933717bad4172858df27e18ca
Kerry Toonen <kerrytoonen@gmail.com>
2016-02-18 11:29:27 -0500

    Initial Commit

---
+++ StretchySnacks/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StretchySnacks
+//
+//  Created by Kerry Toonen on 2016-02-18.
+//  Copyright © 2016 Lighthouse. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
